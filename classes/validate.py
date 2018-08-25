3c644852c511df5563b83f5bbcd607eec55c8b4c
anderson-matheus <matheus5347@gmail.com>
2018-08-25 13:43:02 -0300

    Criação de classe que é responsável por fazer validações

---
+++ classes/validate.py
@@ -0,0 +1,23 @@
+import re
+
+
+class Validate:
+
+    def is_valid_urls(urls):
+        for url in urls:
+            validate = re.compile(
+            	r'(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?'  # http:// or https://
+		        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+		        r'localhost|'  # localhost...
+		        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+		        r'(?::\d+)?'  # optional port
+		        r'(?:/?|[/?]\S+)$', re.IGNORECASE
+            )
+
+            if validate.search(url) == None:
+                return False
+
+        return True
+
+
+
